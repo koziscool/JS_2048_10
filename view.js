29553f932f547dd74a4bae550306e0b1fce20ed5
John Kosmicke <kozjob@yahoo.com>
2019-05-25 19:32:39 -0700

    update

--- view.js
+++ view.js
@@ -1,6 +1,25 @@
 
 view2048 = {
+  model: model2048,
+
+  colors: [ "papayawhip", "darkkhaki", "coral", "lime", "dodgerblue", 
+    "mediumblue", "darkred", "purple", "green", "brown", "black", "orange" ],
+
   init: function(  ) {
-    
+    this.update();
+  },
+
+  update: function( args ) {
+    this.$grid = $("#grid-2048");
+    this.$grid.empty();
+    for( var i = 0; i < this.model.numSquares ; i++ ) {
+      var color = this.colors[0];
+      var value = this.model.values[i] || "&nbsp";
+      if( value > 0 ) color = this.colors[ Math.log2(value) ];
+      var $square = $("<div><div class='value'>" + value + "</div></div>");
+      $square.css("background-color", color);
+      $square.addClass("square");
+      this.$grid.append($square);
+    }
   },
 }
